609e40a27c8985121ef901ca47b598c816ec61ca
Pranay Varshney <pranay.varshney9@gmail.com>
2021-08-20 20:33:06 +0530

    slash bt

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express')
-const  mongoose  = require('mongoose')
+const mongoose = require('mongoose')
 const app = express()
 const port = process.env.PORT || 5000
 const path = require('path')
 const foodRoute = require('./routes/foodRoute')
 const userRoute = require('./routes/userRoute')
 const orderRoute = require('./routes/orderRoute')
-mongoose.connect("mongodb+srv://node-shop:node-shop@node-rest-shop.sttq1.mongodb.net/food-app?retryWrites=true&w=majority",{useUnifiedTopology:true , useNewUrlParser:true})
+mongoose.connect("mongodb+srv://node-shop:node-shop@node-rest-shop.sttq1.mongodb.net/food-app?retryWrites=true&w=majority", { useUnifiedTopology: true, useNewUrlParser: true })
 
 const food = require("./models/foodModel")
 
@@ -32,10 +32,10 @@ app.listen(port, () => console.log(`server on port ${port}!`))
 
 app.use('/api/food', foodRoute);
 app.use('/api/user', userRoute);
-app.use('/api/orders',orderRoute)
+app.use('/api/orders', orderRoute)
 
 
-if(process.env.NODE_ENV === 'production' ){
-    app.use('/',express.static('client/build'))
-    app.get('*', (req, res) => { res.sendFile(path.resolve(__dirname, '/client/build/index.html'))})
+if (process.env.NODE_ENV === 'production') {
+    app.use('/', express.static('client/build'))
+    app.get('*', (req, res) => { res.sendFile(path.resolve(__dirname, 'client/build/index.html')) })
 }
