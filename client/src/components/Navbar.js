2716951b275a15d93307f0350fe4039cc602af4b
Pranay Nandan Varshney <75934932+pranayvarshney@users.noreply.github.com>
2021-08-20 22:08:14 +0530

    Update Navbar.js
    changed name

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -14,7 +14,7 @@ function Navbar() {
     return (
         <>
             <nav className="navbar navbar-expand-lg p-3 mb-5 navbar-light bg-light">
-                <a className="navbar-brand" href="/">Foodict</a>
+                <a className="navbar-brand" href="/">Food.ly</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
